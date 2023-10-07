93cff0255cb14ecef206bdfa4c9543aa3999b562
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-10-08 03:20:54 +0500

    added put request

--- routes/post.mjs
+++ routes/post.mjs
@@ -18,7 +18,6 @@ let router = express.Router();
 
 // POST    /api/v1/post
 router.post("/post", async (req, res, next) => {
-  console.log("this is signup!", new Date());
 
   if (!req.body.title || !req.body.text) {
     res.status(403);
@@ -97,7 +96,7 @@ router.get("/posts", async (req, res, next) => {
       vector: vector,
       // id: "vec1",
       topK: 10000,
-      includeValues: true,
+      includeValues: false,
       includeMetadata: true
   });
 
@@ -105,12 +104,19 @@ router.get("/posts", async (req, res, next) => {
     console.log(`score ${eachMatch.score.toFixed(1)} => ${JSON.stringify(eachMatch.metadata)}\n\n`);
 
   })
-
   console.log(`${queryResponse.matches.length} records found `);
-  console.log("queryResponse: ", queryResponse);
+  
+  const formattedOutput = queryResponse.matches.map(eachMatch => ({
+    text: eachMatch?.metadata?.text,
+    title: eachMatch?.metadata?.title,
+    _id: eachMatch?.id,
+}))
+
+  res.send(formattedOutput);
 
   } catch (err) {
-    console.log(err);
+    console.log("error getting data pinecone: ", err);
+    res.status(500).send('server error, please try later');
     
   }
 });
@@ -137,10 +143,10 @@ router.get("/post/:postId", async (req, res, next) => {
 
 router.put("/post/:postId", async(req, res, next) => {
 
-    if (!ObjectId.isValid(req.params.postId)) {
-        res.status(403).send({ message:"Invalid post id" });
-        return;
-    }
+    // if (!ObjectId.isValid(req.params.postId)) {
+    //     res.status(403).send({ message:"Invalid post id" });
+    //     return;
+    // }
 
     if (!req.body.text && !req.body.title) {
         res.status(403).send(`required parameter missing, atleast one key is required.
@@ -153,26 +159,53 @@ router.put("/post/:postId", async(req, res, next) => {
         `);
     }
 
-    let dataToBeUpdated = {};
+    // let dataToBeUpdated = {};
 
-    if(req.body.title){dataToBeUpdated.title = req.body.title};
-    if(req.body.text){dataToBeUpdated.text = req.body.text};
+    // if(req.body.title){dataToBeUpdated.title = req.body.title};
+    // if(req.body.text){dataToBeUpdated.text = req.body.text};
+
+    // try {
+    //     const updateResponse = await col.updateOne({
+    //         _id: new ObjectId(req.params.postId)
+    //     },
+    //         {
+    //             $set: dataToBeUpdated
+    //         });
+
+    //     console.log("updateResponse : ", updateResponse);
+    
+    //     res.send({ message:"post updated" });
+    //   } catch (err) {
+    //     console.log(" error inserting mongodb : ", err);
+    //     res.status(500).send({ message: "server error, please try later.." });
+    //   }
 
     try {
-        const updateResponse = await col.updateOne({
-            _id: new ObjectId(req.params.postId)
-        },
-            {
-                $set: dataToBeUpdated
-            });
+      const response = await openaiClient.embeddings.create({
+        model: "text-embedding-ada-002",
+        input: `${req.body.title} ${req.body.text}`,
+      });
 
-        console.log("updateResponse : ", updateResponse);
-    
-        res.send({ message:"post updated" });
-      } catch (err) {
-        console.log(" error inserting mongodb : ", err);
-        res.status(500).send({ message: "server error, please try later.." });
-      }
+      const vector = response?.data[0]?.embedding
+      console.log("vector: ", vector);
+
+      const upsertResponse = await pcIndex.upsert([{
+        id: req.params.postId,
+        values: vector,
+        metadata: {
+            title: req.body.title,
+            text: req.body.text,
+          },
+      }]);
+      console.log("upsertResponse: ", upsertResponse);
+
+
+      res.send({ message: 'post created' });
+
+  } catch (e) {
+      console.log("error inserting mongodb: ", e);
+      res.status(500).send({ message: 'server error, please try later' });
+  }
 });
 
 // DELETE  /api/v1/post/:userId/:postId
