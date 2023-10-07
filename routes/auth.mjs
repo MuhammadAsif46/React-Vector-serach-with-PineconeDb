232d6f3ebf92e8d356343f5b9fb0ca5daadb4e88
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-10-07 12:47:22 +0500

    create web

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -5,6 +5,10 @@ const router = express.Router();
 router.post('/login', (req, res, next) => {
     console.log('this is login!', new Date());
     res.send('this is login' + new Date());
+
+
+
+    
 });
 
 router.post("/singup", (req, res, next) => {
